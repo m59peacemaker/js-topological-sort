4e3bd1d01cc366e95f444fe36f7b84060cac0356
Johnny Hauser <johnnyhauser@gmail.com>
2018-09-29 13:04:39 -0500

    implemented dead-end nodes, better api and such, still might be lame

--- index.js
+++ index.js
@@ -1,15 +1,62 @@
-const sort = graph => {
-  const nodeCount = graph.length
-  const sorted = []
+/*
+edgeMap[node].push(edge)
+api to keep or remove edge?
+*/
 
-  while (sorted.length < nodeCount) {
-    const item = graph.shift()
-    const [ node, dependencies ] = item
-    const allDependenciesAreSorted = dependencies.every(dependency => sorted.includes(dependency))
-    allDependenciesAreSorted ? sorted.push(node) : graph.push(item)
+/*
+const sorter = Sorter(edges)
+*/
+
+// Adaptive Topological Sort
+
+// need to distinguish between top level nodes
+// and nodes that require edges, but have only dead-end edges
+
+// TODO: record which edges lead to node (by excluding the dead ones)
+// but that may not be this operation's responsibility
+// probably the consumer of the nodes could:
+// node.edges.filter(edge => !sort.unreachableNodes.includes(edge))
+// and that state could even be maintained outside
+
+const Sort = graph => {
+  const graphQueue = graph.slice(0)
+  const graphSize = graph.length
+  const sortedNodes = new Set()
+  const deadEndNodes = new Set()
+  const unreachableNodes = new Set()
+
+  const isNonTraversableEdge = edge => deadEndNodes.has(edge) || unreachableNodes.has(edge)
+
+  function* sort () {
+    while (sortedNodes.size < (graphSize - unreachableNodes.size)) {
+      const graphItem = graphQueue.shift()
+      const [ node, incomingEdges ] = graphItem
+      const isSourceNode = incomingEdges.length === 0
+      const isUnreachable = !isSourceNode && incomingEdges.every(isNonTraversableEdge)
+
+      if (isUnreachable) {
+        unreachableNodes.add(node)
+      } else {
+        const allTraversableEdgesAreSorted = incomingEdges
+          .filter(edge => !isNonTraversableEdge(edge))
+          .every(edge => sortedNodes.has(edge))
+        if (allTraversableEdgesAreSorted) {
+          sortedNodes.add(node)
+          yield node
+        } else {
+          graphQueue.push(graphItem)
+        }
+      }
+    }
+
+    return sortedNodes
   }
 
-  return sorted
+  return Object.assign(sort(), {
+    graphQueue,
+    sortedNodes,
+    deadEndNodes
+  })
 }
 
-export default sort
+export default Sort
--- index.test.js
+++ index.test.js
@@ -1,5 +1,14 @@
 import test from 'tape'
-import sort from './'
+import Sort from './'
+
+const incomingEdgesPrecedeNode = (callback, graph, sorted) => {
+  const graphMap = new Map(graph)
+  sorted.forEach((node, nodeIndex) => {
+    graphMap.get(node).forEach(incomingEdge => {
+      callback(node, nodeIndex > sorted.indexOf(incomingEdge))
+    })
+  })
+}
 
 test('sort', t => {
   const graph = [
@@ -13,6 +22,43 @@ test('sort', t => {
     [ 2, [ 11 ] ]
   ]
 
-  console.log(sort(graph))
+  const sorted = [ ...Sort(graph) ]
+
+  incomingEdgesPrecedeNode(
+    (node, pass) => t.true(pass, node),
+    graph,
+    sorted
+  )
+
+  t.end()
+})
+
+test('sort with unreachable nodes', t => {
+  const graph = [
+    [ 1, [ ] ],
+    [ 2, [ 1 ] ],
+    [ 3, [ 2 ] ],
+    [ 9, [ ] ],
+    [ 7, [ 6 ] ],
+    [ 4, [ 3 ] ],
+    [ 5, [ 2, 3, 4 ] ],
+    [ 6, [ ] ],
+    [ 10, [ 1, 2, 3, 4 ] ]
+  ]
+
+  const sort = Sort(graph)
+  sort.deadEndNodes.add(2)
+  const sorted = [ ...sort ]
+
+  incomingEdgesPrecedeNode(
+    (node, pass) => t.true(pass, node),
+    graph,
+    sorted
+  )
+
+  // specific order is not necessarily important beyond generally topological sort
+  // it's ok if this changes in that way
+  t.deepEqual(sorted, [ 1, 2, 9, 6, 10, 7 ])
+
   t.end()
 })
